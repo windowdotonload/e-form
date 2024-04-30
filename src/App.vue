93527736af15709a2816bd6cad95ab3e96f471e4
ruichen <rui.chen@inossem.com>
2024-04-30 10:22:47 +0800

    feat(configure prettier): config prettier

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import helloword from "./components/helloword.vue";
+import helloword from './components/helloword.vue'
 </script>
 
 <template>
--- src/components/helloword.vue
+++ src/components/helloword.vue
@@ -1,9 +1,7 @@
 <template>
-  <div>11</div>
+  <div>1 1</div>
 </template>
 
-<script lang="ts" setup>
-
-</script>
+<script lang="ts" setup></script>
 
 <style lang="scss" scoped></style>
